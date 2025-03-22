6d47fed5ef277a94ecaca925fa6f297caa380cec
joeyissa <joeyissa59@gmail.com>
2025-03-22 18:30:13 -0400

    more stuff relating to elements

--- Game/include/Element.h
+++ Game/include/Element.h
@@ -4,11 +4,14 @@
 class Element {
 public:
     Element();
-    Element(bool movable, bool dynamic, bool flammable);
+    Element(bool movable, bool dynamic, bool flammable, int density, int gravity);
 
     bool isMovable() const;
     bool isDynamic() const;
     bool isFlammable() const;
+    int density() const; // check when you want to see what should 'float' to the surface
+    int gravity() const; // difference in movement direction (water vs gas)
+
 
 private:
     bool movable;
--- Game/source/Element.cpp
+++ Game/source/Element.cpp
@@ -2,10 +2,10 @@
 
 // default acts like air: movable, dynamic, and not flammable
 Element::Element() 
-    : movable(true), dynamic(true), flammable(false) {}
+    : movable(true), dynamic(true), flammable(false), density(1), gravity(0)  {}
 
-Element::Element(bool movable, bool dynamic, bool flammable)
-    : movable(movable), dynamic(dynamic), flammable(flammable) {}
+Element::Element(bool movable, bool dynamic, bool flammable, int density, int gravity)
+    : movable(movable), dynamic(dynamic), flammable(flammable), density(density), gravity(gravity) {}
 
 bool Element::isMovable() const {
     return movable;
